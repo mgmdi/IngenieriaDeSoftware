84fd3cb3c8e8deeb0870461b09bfcc551eecffd1
14-10526 <14-10526@usb.ve>
2018-10-11 12:08:56 -0400

    Se agregaron las pruebas para comprobar el funcionamiento de la funcion principal a traves de unit test

---
+++ testFinal.py
@@ -0,0 +1,18 @@
+import unittest
+import seguros
+
+
+class TestFinal(unittest.TestCase):
+    '''
+    Funcion que realiza la prueba de la ejecucion del programa completo.
+    Los datos pueden ser ingresados mediante un archivo de texto o mediante consola.
+    '''
+    def test_programa(self):         
+    
+        #CASO 1  // Para ingresar datos que cumplen con los requisitos 
+        self.assertEqual(seguros.main(),"SI")
+        #CASO 2  // Para ingresar datos que no cumplan con los requisitos 
+        self.assertEqual(seguros.main(),"NO")
+
+if __name__ == '__main__':
+    unittest.main()  
