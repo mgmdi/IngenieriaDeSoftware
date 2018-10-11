0d4859f2add1b8a886b82c219696f48a7def3be0
mgmdi <14-10444@usb.ve>
2018-10-10 20:40:27 -0400

    test_beta V2: se agregan pruebas maliciosas con formato de fecha incorrecto y parametros incorrectos

--- test_beta.py
+++ test_beta.py
@@ -5,11 +5,11 @@ class TestBeta(unittest.TestCase):
 
     def test_calculoEdad(self):
      
-        self.assertEqual(seguros.calculoEdad("16/06/1997"), 21)
+        self.assertEqual(seguros.calculoEdad("16061997"), 21)
 
     def test_verificacionDatos(self):
 
-        self.assertEqual(seguros.verificacionDatos(56,"f",800), "SI")
+        self.assertEqual(seguros.verificacionDatos(56,"l","m"), "SI")
 
 if __name__ == '__main__':
     unittest.main() 
