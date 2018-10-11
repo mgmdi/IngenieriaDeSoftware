5fbebdcf113fd61a881b0e61b102099f549ef965
mgmdi <14-10444@usb.ve>
2018-10-10 21:13:02 -0400

    test_beta V3: se prueba que se salga del programa si los datos no son estan en el formato correcto. Se procede a la edicion del codigo para que pase la prueba.

--- test_beta.py
+++ test_beta.py
@@ -5,11 +5,19 @@ class TestBeta(unittest.TestCase):
 
     def test_calculoEdad(self):
      
-        self.assertEqual(seguros.calculoEdad("16061997"), 21)
+        with self.assertRaises(SystemExit) as cm:
+            seguros.calculoEdad("16061997")
+
+        self.assertEqual(cm.exception.code, 1)
 
     def test_verificacionDatos(self):
 
-        self.assertEqual(seguros.verificacionDatos(56,"l","m"), "SI")
+        self.assertEqual(seguros.verificacionDatos(56,"f",800), "SI")
+        with self.assertRaises(SystemExit) as cm:
+            seguros.verificacionDatos(56,"l","m")
+
+        self.assertEqual(cm.exception.code, 1)
+
 
 if __name__ == '__main__':
     unittest.main() 
